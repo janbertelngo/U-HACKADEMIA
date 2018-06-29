7c2dc47d39a34b6f35be38d8c5eded00a5047c77
ACBatosalem <angelika_batosalem@dlsu.edu.ph>
2018-06-30 00:04:06 +0800

    query for insert docotr

--- models/doctorService.js
+++ models/doctorService.js
@@ -3,5 +3,16 @@ let database = require('./../configuration/database')
 let GET_DOCTOR_QUERY = "SELECT * FROM DoctorAccount"
 
 let INSERT_DOCTOR_QUERY = "INSERT INTO DoctorAccount " +
-                    "(firstName, lastName, username, password, birtdate, + "
+                    "(firstName, lastName, username, password, birthdate, + "
                     + "sex, workplace) VALUES (?, ?, ?, ?, ?, ?, ?)"	
+
+module.exports.createDoctor = function (data, next) {
+    let db = database.getDBInstance()
+    db.run(INSERT_DOCTOR_QUERY, 
+        [data.firstName, data.lastName, data.username, data.password], 
+        function(error) {
+        if (error) 
+            return next({status: 'error', data: error})
+        next({status: 'success', data: this.lastID})
+    })
+}
