694c0ab822eb8b85751c4a8efac5fd2feff5cddf
Neilson Keith P. Gardose <neilsongardose@gmail.com>
2018-06-30 03:23:52 +0800

    Userview update. Doctor

--- controllers/index.js
+++ controllers/index.js
@@ -11,4 +11,7 @@ module.exports.controller = function(app) {
     app.get('/patient/1', function(req, res) {
         res.render('userview')
     });
+    app.get('/doctor/1', function(req,res) {
+        res.render('doctorview');
+    });
 }
