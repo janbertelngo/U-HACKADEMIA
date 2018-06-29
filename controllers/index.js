a8875a183db415eb5658eb7492f3b405d3c7e42e
Your Name <you@example.com>
2018-06-30 03:11:15 +0800

    hello

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,18 @@
+var path = require('path');
+var express = require('express');
+
 module.exports.controller = function(app) {
+    app.use(express.static(path.join(__dirname, 'public')));
     app.get('/', function(req, res) {
         res.render('index')
     });
     app.get('/signup', function(req, res) {
         res.render('signup')
     });
+    app.get('/lab', function(req, res) {
+        res.render('addrecord')
+    }); 
+    app.get('/lab/1/addrecord', function(req, res) {
+        res.render('addrecord')
+    }); 
 }
