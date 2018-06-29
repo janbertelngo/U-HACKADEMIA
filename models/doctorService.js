eabde366d157ac9826770b1835398732cd59afa2
jnllgaba <janelle_gaba@dlsu.edu.ph>
2018-06-30 00:02:51 +0800

    aaaaa

--- models/doctorService.js
+++ models/doctorService.js
@@ -4,4 +4,4 @@ let GET_DOCTOR_QUERY = "SELECT * FROM DoctorAccount"
 
 let INSERT_DOCTOR_QUERY = "INSERT INTO DoctorAccount " +
                     "(firstName, lastName, username, password, birtdate, + "
-                    + "sex, workplace) VALUES (?, ?, ?, ?, ?, ?, ?)"	
+                    + "sex, workplace) VALUES (?, ?, ?, ?, ?, ?, ?)"
--- models/labService.js
+++ models/labService.js
@@ -4,4 +4,5 @@ let GET_LAB_QUERY = "SELECT * FROM LabAccount"
 
 let INSERT_LAB_QUERY = "INSERT INTO LabAccount " +
                     "(firstName, lastName, username, password, birtdate, + "
-                    + "sex) VALUES (?, ?, ?, ?, ?, ?)"	
+                    + "sex) VALUES (?, ?, ?, ?, ?, ?)"
+
